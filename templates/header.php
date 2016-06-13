788948dd41e9dc035ddb732e7d9f2671c1063ab8
Matt Rothenberg <mattrothenberg@gmail.com>
2016-06-12 19:48:16 -0700

    Finish header layout

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,22 @@
-<header class="banner">
+<nav class="navbar navbar-default">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/assets/images/abxy-logo.jpg' ?>" alt="" />
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
       endif;
       ?>
-    </nav>
+    </div>
   </div>
-</header>
+</nav>
