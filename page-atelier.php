361f8cf5bcff864fdbd70fd620f3476630cd51c3
Matt Rothenberg <mattrothenberg@gmail.com>
2016-06-12 20:38:20 -0700

    More tweaks to artist row

--- page-atelier.php
+++ page-atelier.php
@@ -39,6 +39,26 @@
       <button class="btn btn-small btn-transparent" href="#">Download Artist's Press Pack</button>
     </div>
   </div>
+  <div class="row artist-row">
+    <div class="artist-image">
+      <img class="img-responsive" src="http://placekitten.com/700/500" alt="" />
+    </div>
+    <div class="artist-bio">
+      <h2>Artist Name</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button class="btn btn-small btn-transparent" href="#">Download Artist's Press Pack</button>
+    </div>
+  </div>
+  <div class="row artist-row">
+    <div class="artist-image">
+      <img class="img-responsive" src="http://placekitten.com/700/500" alt="" />
+    </div>
+    <div class="artist-bio">
+      <h2>Artist Name</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button class="btn btn-small btn-transparent" href="#">Download Artist's Press Pack</button>
+    </div>
+  </div>
 
   <hr>
 <?php endwhile; ?>
