ea1e41e7f2a524491c393a42349ecaa6ce0fb277
Matt Rothenberg <mattrothenberg@gmail.com>
2016-06-15 09:28:14 -0700

    Finish Private Client page

--- page-private-client.php
+++ page-private-client.php
@@ -2,14 +2,13 @@
   <div class="floating-panel private-panel rellax" data-rellax-speed="1"></div>
   <div class="row">
     <div class="col-xs-12 col-sm-5 sm-top-offset-75">
-      <h1 class="text-right"><?php the_title(); ?></h1>
-      <div class="lead">
-        <?php get_template_part('templates/content', 'page'); ?>
-      </div>
+      <h1 class="heading-sm-right"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/content', 'page'); ?>
     </div>
     <div class="col-xs-12 col-sm-7">
-      <img class="img-responsive" src="http://placekitten.com/700/500" alt="" />
-      <em class="em-subheading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</em>
+      <?php if ( has_post_thumbnail() ) {
+        the_post_thumbnail('full', array( 'class' => 'img-responsive' ));
+      } ?>
     </div>
   </div>
 <?php endwhile; ?>
