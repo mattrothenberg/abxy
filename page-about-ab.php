da92fac6563b16b7d767d337dd37a5bbd9cf09e3
Matt Rothenberg <mattrothenberg@gmail.com>
2016-06-16 17:42:41 -0700

    More visual design tweaks

--- page-about-ab.php
+++ page-about-ab.php
@@ -1,7 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="floating-panel about-panel rellax" data-rellax-speed="1"></div>
-  <div class="row">
-
+  <div class="row about-ab-content">
     <div class="col-xs-12 col-sm-7 sm-top-offset-small mbxl">
       <h1><?php the_title(); ?></h1>
       <?php if ( has_post_thumbnail() ) {
--- page-contact.php
+++ page-contact.php
@@ -1,6 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <div class="floating-panel contact-panel rellax" data-rellax-speed="1"></div>
-  <div class="row">
+  <div class="row contact-page-content">
     <div class="col-xs-12 col-sm-3 col-sm-offset-2">
       <h1><?php the_title(); ?></h1>
       <?php get_template_part('templates/content', 'page'); ?>
